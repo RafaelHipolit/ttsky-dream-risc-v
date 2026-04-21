fa9b6556cbc874e0f5f34729771e1431b9e9a91e
Rafael Hipólito <64715008+RafaelHipolit@users.noreply.github.com>
2026-04-20 22:39:40 -0300

    remove uma ganbiarra que deu errado

--- src/zeroriscy_cs_registers.sv
+++ src/zeroriscy_cs_registers.sv
@@ -106,13 +106,6 @@ module zeroriscy_cs_registers
   `define MSTATUS_MPP_BITS    12:11
 
 
-  typedef enum logic[1:0] {
-  PRIV_LVL_M = 2'b11,
-  PRIV_LVL_H = 2'b10,
-  PRIV_LVL_S = 2'b01,
-  PRIV_LVL_U = 2'b00
-} PrivLvl2_t;
-
 
   typedef struct packed {
     //logic uie;       - unimplemented, hardwired to '0
@@ -125,7 +118,7 @@ module zeroriscy_cs_registers
     logic mpie;
     // logic spp;      - unimplemented, hardwired to '0
     // logic[1:0] hpp; - unimplemented, hardwired to '0
-    PrivLvl2_t mpp;
+    PrivLvl_t mpp;
   } Status_t;
 
   // Performance Counter Signals
@@ -207,7 +200,7 @@ module zeroriscy_cs_registers
           //mstatus_n = '{
           //mie:  csr_wdata_int[`MSTATUS_MIE_BITS],
           //mpie: csr_wdata_int[`MSTATUS_MPIE_BITS],
-          //mpp:  PrivLvl2_t'(PRIV_LVL_M)
+          //mpp:  PrivLvl_t'(PRIV_LVL_M)
           //};
       end
       // mepc: exception program counter
